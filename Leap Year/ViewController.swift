583001d5bcc4074bd4f7f62e16936896e4bed798
jpurnell <jpurnell@Wheeljack.local>
2014-10-10 15:00:49 -0400

    Version 1.0

--- Leap Year/ViewController.swift
+++ Leap Year/ViewController.swift
@@ -3,13 +3,15 @@
 //  Leap Year
 //
 //  Created by Justin Purnell on 10/10/14.
-//  Copyright (c) 2014 JP Enterprises, Inc. All rights reserved.
+//  Copyright © 2014 JP Enterprises, Inc. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func yearEnteredAction(yearTextField: UITextField) {
+        var inputYear = yearTextField.text
+        var testYear = inputYear.toInt()
+        if testYear == nil {
+            yearLabel.text = "Try a number?"} else {
+
+            if (testYear! % 4 == 0 && !(testYear! % 100 == 0 && testYear! % 400 != 0)){
+                yearLabel.text = "LEAP YEAR!"
+            } else {
+                yearLabel.text = "Normal year."
+        }
+        }
+    }
 }
 

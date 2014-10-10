9ee0db949f857b39ccbbc23a799afa1a74759f01
jpurnell <jpurnell@Wheeljack.local>
2014-10-10 10:16:36 -0400

    Initial Commit

---
+++ Leap Year/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leap Year
+//
+//  Created by Justin Purnell on 10/10/14.
+//  Copyright (c) 2014 JP Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
